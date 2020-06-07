85f9c7fe20a673ce6f778a0f9ae70a4cf05d0ce0
Mercy <mercyemeth@gmail.com>
2020-06-07 02:36:47 -0400

    hides resume for time-being

--- src/App.js
+++ src/App.js
@@ -32,23 +32,18 @@ function App() {
         <span className="intro">~& the Multipotentiality of &~
         </span>
         <div className="empty_space"></div>
-        <div className="potentialities" onClick="showResume"> <a href="#resume"> Résumé</a></div>
         <div className="down_arrow">v</div>
         </div>
         <div className="empty_space"></div>
         <div className="resume" id="resume" >
         <div className="resume_title">
-          <h2>Mercy Odhner</h2>
-      </div>
-      <hr />
-      <p>Tenacious <a href="https://en.wikipedia.org/wiki/Multipotentiality"
-      >multipotentialite</a> who excels in problem identification and crafting creative solutions across fields and platforms; strong aptitude in applying both hard and soft skills and adapting to scale and scope</p>
-      <hr />
-      <h3>TECHNICAL EDUCATION</h3>
-      <p>Academy Pgh —​ ​ A 12-week, demand-driven bootcamp in which self-managed teams study and work in: Object-oriented programming and web development, employing:</p>
-      <h5>Python3 • C# • Ruby • Rails • React • React Native • ASP.NET MVC • Ajax • JavaScript • HTML • CSS • Visual Studio Code • Visual Studio • Git • SQL • SQLight • PostgreSQL • Agile development • unit testing • TDD • RSpec • Selenium • Cucumber • APIs • SEO & SEM</h5>
-      <p>to build programs from “Hello World” to fully-functional websites and applications such as a client-commissioned mobile app for business accessibility and door security.</p>
-      <h3>CONTRACT BASED WORK EXPERIENCE</h3>
+              </div>
+      <p>---</p>
+      <p>Developer</p>
+      <p>Writer</p>
+      <p>Educator</p>
+      <p>Linguist</p>
+      {/* <h3></h3>
       <p className="position">Copy Editor — Smith &​ Diction ||​ Philadelphia <span className="dates">May ‘19 – Present</span></p>
       <p> • Research and write compelling copy for websites, ad campaigns, branding material and brochures</p>
       <p className="position">Real Estate Agent — RE/MAX Select Realty || East End, Pittsburgh <span className="dates">Jan ‘19 – Present</span></p>
@@ -72,9 +67,8 @@ function App() {
       <p className="position">Crew Member — Spak Brothers Pizza || Pittsburgh, PA <span className="dates">Oct ‘16 – Oct ‘18</span></p>
       <p>• Devised and revamped systems for order placement and fulfillment   </p>
       <p>• Developed and integrated methods for tracking profitability and learning from instances of failure</p>
-      <p>• Spearheaded training and implementation of a new POS system</p>
-      <p> </p>
-      <hr />
+      <p>• Spearheaded training and implementation of a new POS system</p>*/}
+      <p>---</p>
       </div>
       <div className="footer_links">
       {/* <span className="intro_mirror"> • ~ & t h e M u l t i p o t e n t i a l i t y  o f & ~ </span> */}
@@ -83,7 +77,6 @@ function App() {
         <a className="footer_links" href="https://codepen.io/mercycoder">CodePen</a>
         {/* <span className="intro"> • ~  & t h e   M u l t i p o t e n t i a l i t y  o f  &  ~  • </span> */}
         </div>
-      <hr />
 
       <div className="buffer">      
       </div> 
